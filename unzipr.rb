b8aa16f5fbcb6e07461a4503cd3b522b29f0fc66
Taivo Käsper <taivo.kasper@gmail.com>
2018-02-27 10:54:27 +0200

    Version increase

--- unzipr.rb
+++ unzipr.rb
@@ -1,9 +1,9 @@
 class Unzipr < Formula
-  desc "Utility for listing files from zip of zip of zips"
+  desc "Utility for listing and unpacking contents from nested zip archives"
   homepage "https://github.com/taivokasper/unzipr"
-  url "https://github.com/taivokasper/unzipr/releases/download/v0.3/unzipr-Darwin"
-  sha256 "963705488fd7f11eeed3d69cb8bf0543dff85b138f855f0e629152bfda4f1e52"
-  version "0.3.0"
+  url "https://github.com/taivokasper/unzipr/releases/download/v0.3.1/unzipr-Darwin"
+  sha256 "0080bd004e5144ac3049b5c27c75a7f3f5157e8af3f190841c77a54a140c3dee"
+  version "0.3.1"
   revision 1
   def install
     mv "unzipr-Darwin", "unzipr"
